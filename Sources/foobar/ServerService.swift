abe6fc2f7b56bd15009095062e84287b2fc4ed6b
Glenn Sequeira <glenn@sequeiralabs.com>
2026-08-11 16:57:41 +0300

    Integrated the API handler with routes

--- Sources/foobar/ServerService.swift
+++ Sources/foobar/ServerService.swift
@@ -1,8 +1,21 @@
 import Vapor
 import ServiceLifecycle
+import OpenAPIVapor
 
 func configureServer(_ application: Application) async throws -> Service {
     routes(application)
+
+    // Create API handler for request processing
+    let handler = APIHandler()
+
+    // Register OpenAPI-generated handlers with Vapor transport
+    let transport = VaporTransport(routesBuilder: application)
+    try handler.registerHandlers(
+        on: transport,
+        serverURL: Servers.Server1.url(),
+        configuration: .init()
+    )
+
     return ServerService(application: application)
 }
 

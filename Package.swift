df8e527a7ac0c7ad22367d2b632bec0b9670c245
Glenn Sequeira <glenn@sequeiralabs.com>
2026-08-11 17:01:37 +0300

    Added the Fluent dependencies

--- Package.swift
+++ Package.swift
@@ -12,6 +12,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.13.0"),
         .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.12.0"),
         .package(url: "https://github.com/vapor/swift-openapi-vapor.git", from: "1.1.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.13.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.9.0")
     ],
     targets: [
         .executableTarget(
@@ -22,6 +24,8 @@ let package = Package(
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
                 .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver")
             ],
             plugins: [
                 .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")

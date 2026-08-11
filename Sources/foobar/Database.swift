a9b537d138a87ed81e4b557a004dc7fbae16a83a
Glenn Sequeira <glenn@sequeiralabs.com>
2026-08-11 17:30:37 +0300

    Registered the migration

--- Sources/foobar/Database.swift
+++ Sources/foobar/Database.swift
@@ -24,7 +24,7 @@ func configureDatabase(application: Application) async throws {
 
         // Add migrations
         application.migrations.add([
-
+            Migrations.CreatePolls()
         ])
 
         // Run migrations automatically
